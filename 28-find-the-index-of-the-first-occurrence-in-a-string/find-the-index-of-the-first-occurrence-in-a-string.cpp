234a8953a18bdeb3f5bb36c51c3bf236341b5da6
Sumitshukla4020 <120705512+Sumitshukla4020@users.noreply.github.com>
2024-09-02 23:23:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (24.77%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -4,20 +4,16 @@ public:
         if (needle.empty()) {
             return 0;
         }
-
-        // Get the length of haystack and needle
         int haystackLen = haystack.length();
         int needleLen = needle.length();
 
-        // Loop through the haystack to find the first occurrence of needle
         for (int i = 0; i <= haystackLen - needleLen; i++) {
-            // Check if the substring of haystack starting at index i matches the needle
+            
             if (haystack.substr(i, needleLen) == needle) {
                 return i;
             }
         }
 
-        // If needle is not found, return -1
         return -1;
     }
 };
