ab51ccaf41c76f911673b8d5ccf02892d417fa7a
Sumitshukla4020 <120705512+Sumitshukla4020@users.noreply.github.com>
2024-09-13 10:31:36 +0530

    Time: 60 ms (61.22%) | Memory: 42 MB (53.29%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        vector<int> ans;
-        for(int i=0;i<queries.size();i++){
-            int a = queries[i][0];
-            int b = queries[i][1];
-            int k = 0;
-            while(a<=b){
-                k = k^arr[a++];
-            }
-            ans.push_back(k);
+        // vector<int> ans;
+        // for(int i=0;i<queries.size();i++){
+        //     int a = queries[i][0];
+        //     int b = queries[i][1];
+        //     int k = 0;
+        //     while(a<=b){
+        //         k = k^arr[a++];
+        //     }
+        //     ans.push_back(k);
+        // }
+        // return ans;
+
+        int n = arr.size();
+        vector<int> prefix(n);
+        prefix[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i-1] ^ arr[i];
         }
+        
+        vector<int> ans;
+        for (const auto& q : queries) {
+            int a = q[0];
+            int b = q[1];
+            if (a == 0) {
+                ans.push_back(prefix[b]);  
+            } else {
+                ans.push_back(prefix[b] ^ prefix[a-1]);  
+            }
+        }
+        
         return ans;
     }
 };
